b47194768edbe66286df4f6332b268fe61aa5bef
SHSong <99pyues2@gmail.com>
2024-04-30 22:21:33 +0900

    feature : 도메인1

--- backend/src/main/java/wap/dingdong/backend/domain/User.java
+++ backend/src/main/java/wap/dingdong/backend/domain/User.java
@@ -3,16 +3,22 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
 @Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
 @Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
     @Column(nullable = false)
@@ -35,4 +41,11 @@ public class User {
     private AuthProvider provider;
 
     private String providerId;
+
+    //연관관계 매핑
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    List<Product> products = new ArrayList<>();
+
+
 }
---
+++ backend/src/main/java/wap/dingdong/backend/domain/WishList.java
@@ -0,0 +1,15 @@
+package wap.dingdong.backend.domain;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class WishList {
+}
