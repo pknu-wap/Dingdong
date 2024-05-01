6b71a53c6b5aaeb81f9f15f72be9ed6bcdfbf9d5
이동훈 <134472292+Hun0906@users.noreply.github.com>
2024-05-02 00:16:53 +0900

    refactor : 메인페이지 코드수정#2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import {useState} from "react"
 import './App.css';
 import Header from './Header';
-import Nav from './Navigator';
-import Product from './ProductList';
-
-
-const App = () => {
-    return (
-        <div className="app">
-            <Header />
-            <Nav />
-            <Product />
-        </div>
-    );
-};
+import Productlist from'./ProductList';
+import Mypage from'./Mypage';
+import Productreg from'./Productreg';
+import { Route, Routes,useNavigate } from "react-router-dom"; 
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Routes>
+     <Route path="/productlist" element={<Productlist/> } />
+     <Route path="/productreg" element={<Productreg/>}/>
+     <Route path="/mypage" element={<Mypage/> } />
+     </Routes>
+    </div>
+  );
+}
 
 export default App;
--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,21 +1,52 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Route, Routes,useNavigate } from "react-router-dom"; 
 
 const Header = () => {
+  let navigate=useNavigate();
     return (
-        <header>
-            <div className = 'Logo'>
-                <img src = "logo.png" alt="딩동" style={{width : '30px', height: ' auto'}}/>딩동
+        <div>
+            <Container fluid className='header-size'>
+             <Row>
+        <Col className='col-3'>
+        <img src = "dingdong로고.png" style={{width : '40px', height: 'auto',marginBottom:'10px'}}/>
+        <span style={{ fontSize: '30px' ,fontWeight:'bold',textAlign:'center'}}>딩동</span>
+        </Col>
+
+        <Col className='col-3'>
+        <input class="form-control mr-sm-2" type="search" placeholder="검색어를 입력하세요" aria-label="Search"/>
+        </Col>
+
+        <Col className='col-1'>
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </Col>
+
+        <Col className='col-2'>
+        <img src="지도로고.png" alt="지도 아이콘" style={{width:'40px',height:'auto',marginBottom:'20px',marginLeft:'10px'}}/>
+        </Col>
+        
+        <Col className='col-3'>
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">로그인/회원가입</button>
+        </Col>
+      </Row>
+      </Container>
+   
+     
+      <Navbar className="bg-body-tertiary">
+      <Container>
+      
+        <Navbar.Brand onClick={()=>{navigate('/productlist')}}>전체상품목록</Navbar.Brand>
+        <Navbar.Brand onClick={()=>{navigate('/productreg')}}>상품판매</Navbar.Brand>
+        <Navbar.Brand  onClick={()=>{navigate('/mypage')}}>마이페이지</Navbar.Brand>
+       
+      </Container>
+    </Navbar>
+            
             </div>
-            <div className="search-bar">
-                 <input type="text" placeholder="검색어를 입력하세요" />
-            </div>
-            <div className="location-selector">
-                <img src="map-icon.png" alt="지도 아이콘" style={{width:'20px',height:'auto'}}/>
-            </div>
-            <div className="user-options">
-                <button>로그인 / 회원가입</button>
-            </div>
-        </header>
     );
 };
 
