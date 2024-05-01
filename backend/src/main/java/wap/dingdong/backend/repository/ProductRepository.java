d6985bd380ec540f083916c5d90e2317f1650078
ash <ash131623@gmail.com>
2024-05-02 00:28:42 +0900

    feature : 상품조회 페이지

---
+++ backend/src/main/java/wap/dingdong/backend/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package wap.dingdong.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import wap.dingdong.backend.domain.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
