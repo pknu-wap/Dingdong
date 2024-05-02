a0b903fd29c6c5c1072d3fed1780d55c4113b79b
ash <ash131623@gmail.com>
2024-05-02 15:22:09 +0900

    feature : 저장, 수정, 조회

--- backend/src/main/java/wap/dingdong/backend/controller/ProductController.java
+++ backend/src/main/java/wap/dingdong/backend/controller/ProductController.java
@@ -1,38 +1,39 @@
 package wap.dingdong.backend.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import wap.dingdong.backend.domain.Product;
+import wap.dingdong.backend.dto.ProductResponseDto;
+import wap.dingdong.backend.dto.ProductSaveRequestDto;
+import wap.dingdong.backend.dto.ProductUpdateRequestDto;
 import wap.dingdong.backend.service.ProductService;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Controller
 public class ProductController {
-    @Autowired
-    private ProductService productService;
 
-    @GetMapping("/products")
-    public String getAllProducts(Model model) {
-        List<Product> products = productService.getAllProducts();
-        model.addAttribute("products", products);
-        return "products";
+    private final ProductService productService;
+
+    // 게시글 저장, id return
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody ProductSaveRequestDto requestDto) {
+        return productService.save(requestDto);
     }
 
-    @GetMapping("/products/new")
-    public String getNewProductForm(Model model) {
-        // 적절한 기본값을 가진 객체를 생성하여 모델에 추가
-        model.addAttribute("product", new Product());
-        return "productForm";
+    //게시글 수정
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody ProductUpdateRequestDto requestDto){
+        return productService.update(id, requestDto);
     }
 
-    @PostMapping("/products")
-    public String addProduct(@ModelAttribute Product product) {
-        productService.addProduct(product);
-        return "redirect:/products";
+    //게시글 조회
+    @GetMapping("/api/v1/posts/{id}")
+    public ProductResponseDto findById(@PathVariable Long id) {
+        return productService.findById(id);
     }
 }
