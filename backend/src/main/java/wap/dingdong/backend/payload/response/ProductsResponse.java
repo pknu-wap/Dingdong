0a426c318569df4cf3ea926335c812f86cf51219
SHSong <99pyues2@gmail.com>
2024-05-03 15:30:19 +0900

    fix : 버그수정

--- backend/src/main/java/wap/dingdong/backend/payload/response/ProductsResponse.java
+++ backend/src/main/java/wap/dingdong/backend/payload/response/ProductsResponse.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class ProductsResponse {
+public class ProductsResponse{
 
     private List<ProductInfoResponse> productsResponse;
 
