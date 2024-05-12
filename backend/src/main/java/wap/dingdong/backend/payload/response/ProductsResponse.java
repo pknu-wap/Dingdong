5d45655f8d1d9adc1cc7b7f88368e80b4140e7dd
SHSong <99pyues2@gmail.com>
2024-05-13 01:53:47 +0900

    refactor : 서비스 리팩토링

--- backend/src/main/java/wap/dingdong/backend/payload/response/ProductsResponse.java
+++ backend/src/main/java/wap/dingdong/backend/payload/response/ProductsResponse.java
@@ -4,8 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import wap.dingdong.backend.domain.Product;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter @Setter
 @AllArgsConstructor
@@ -14,5 +16,13 @@ public class ProductsResponse{
 
     private List<ProductInfoResponse> productsResponse;
 
+    //리스트를 반환해야 함으로 상품 리스트의 각 요소들을 ProductInfoResponse.of 메서드의 매개변수에 넣어 변환하고 리스트로 만듬
+    public static List<ProductInfoResponse> of(List<Product> products) {
+        return products.stream()
+                .map(ProductInfoResponse::of)
+                .collect(Collectors.toList());
+    }
+
 }
+
 
