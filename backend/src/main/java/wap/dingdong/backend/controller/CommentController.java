2add65e767b987272e89c0c0a3330678a1fd9d90
ash <ash131623@gmail.com>
2024-05-18 20:57:01 +0900

    feature : 댓글 조회

--- backend/src/main/java/wap/dingdong/backend/controller/CommentController.java
+++ backend/src/main/java/wap/dingdong/backend/controller/CommentController.java
@@ -3,13 +3,12 @@ package wap.dingdong.backend.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import wap.dingdong.backend.payload.request.CommentRequest;
 import wap.dingdong.backend.payload.response.CommentResponse;
 import wap.dingdong.backend.service.ProductService;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,6 +17,12 @@ public class CommentController {
     @Autowired
     private ProductService productService;
 
+    // 댓글 조회
+    @GetMapping("/product/{productId}/comment")
+    public ResponseEntity<List<CommentResponse>> getAllCommentsForBoard(@PathVariable Long productId) {
+        List<CommentResponse> comments = productService.getAllCommentsForBoard(productId);
+        return ResponseEntity.ok(comments);
+    }
     // 댓글 생성
     @PostMapping("/product/{productId}/comment")
     public ResponseEntity<CommentResponse> createComment(
