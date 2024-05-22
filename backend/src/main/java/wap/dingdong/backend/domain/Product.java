5c8a3112dc2544efc447c06000e05fd6d75c0a65
ash <ash131623@gmail.com>
2024-05-22 16:17:11 +0900

    feature : 찜 컬럼 수정 & createAt 변수명 및 타입 수정

--- backend/src/main/java/wap/dingdong/backend/domain/Product.java
+++ backend/src/main/java/wap/dingdong/backend/domain/Product.java
@@ -5,12 +5,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -29,11 +33,17 @@ public class Product {
     @Enumerated(EnumType.STRING)
     private ProductStatus status = ProductStatus.ON_SALE; //기본값 ON_SALE
 
-    @Enumerated(EnumType.STRING)
-    private ProductLike productLike = ProductLike.UNLIKE; //기본값 UNLIKE
+    // 찜 - 기존 코드
+//    @Enumerated(EnumType.STRING)
+//    private ProductLike productLike = ProductLike.UNLIKE; //기본값 UNLIKE
 
-    @CreatedDate
-    private LocalDateTime createdAt;
+    // 찜 - 수정
+    @Column(columnDefinition = "integer default 0")
+    private Integer productLike; // 상품 찜 수
+
+    // 어노테이션, 데이터타입, 변수명 수정
+    @CreationTimestamp
+    private Timestamp productRegDate;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
@@ -65,5 +75,36 @@ public class Product {
         this.contents = contents;
         this.images = images;
         this.locations = locations;
+    }
+
+
+    /* ------------- 상품 찜하기 메소드 -------------- */
+
+    // 찜하기 클릭한 사용자 이메일 저장
+    @ElementCollection
+    @CollectionTable(name = "product_likes", joinColumns = @JoinColumn(name = "product_id"))
+    @Column(name = "user_id")
+    private Set<String> likedByMembers = new HashSet<>(); // 좋아요를 누른 사용자 이메일 저장
+
+
+    public void increaseLike(String user_id) {
+        if (!likedByMembers.contains(user_id)) {
+            productLike++;
+            likedByMembers.add(user_id);
+        }
+    }
+
+    public void decreaseLike(String user_id) {
+        if (likedByMembers.contains(user_id)) {
+            productLike--;
+            likedByMembers.remove(user_id);
+        }
+    }
+
+    public boolean isLikedByMember(String user_id) {
+        if (this.user == null || this.likedByMembers.isEmpty()) {
+            return false;
+        }
+        return this.likedByMembers.contains(user_id);
     }
 }
